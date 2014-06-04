921486b111018880ebd0d6a8df457fec0227dc76
iOS Apps Development <info@iosappsdev.org>
2014-06-03 23:36:54 -0700

    Initial Commit

--- syntaxReview/ViewController.m
+++ syntaxReview/ViewController.m
@@ -8,7 +8,11 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () {
+    int counter;
+    NSString *myString1;
+    NSString *myString2;
+}
 
 @end
 
@@ -18,12 +22,59 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    counter = 300;
+    myString1 = @"Richard";
+    myString2 = @"Ricardo";
+    
+    [self myMethod];
+    [self myMethod2];
+    [self myMethod3];
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)myMethod {
+    
+    switch (counter) {
+        case 300:
+            NSLog(@"Counter Value = %i", counter);
+            break;
+        case 301: {
+            
+        }
+            break;
+            
+        default:
+            break;
+    }
+    
 }
+
+-(void)myMethod2 {
+    
+    if (myString1 != myString2) {
+        NSLog(@"Strings are not the same");
+    } else if (myString1 == myString2) {
+        NSLog(@"Strings are the same");
+    } else {
+        
+    }
+}
+
+-(void)myMethod3 {
+    
+    for (int i = 298; i <= counter; i++) {
+        
+        if (i == 300) {
+            NSLog(@"You Made it");
+        } else {
+            NSLog(@"You are almost there.");
+        }
+        
+    }
+    
+    
+    
+}
+
 
 @end
